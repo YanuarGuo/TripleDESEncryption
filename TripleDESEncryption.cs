c6e038fe60ddbf3d47dfbbc2ace5492f05cf308f
YanuarGuo <yanuar.utama14@gmail.com>
2025-03-18 09:25:19 +0700

    Add project files.

---
+++ TripleDESEncryption.cs
@@ -0,0 +1,10 @@
+namespace TripleDESEncryption
+{
+    public partial class TripleDESEncryption : Form
+    {
+        public TripleDESEncryption()
+        {
+            InitializeComponent();
+        }
+    }
+}
