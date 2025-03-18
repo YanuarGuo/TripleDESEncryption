80c6cfdc1cf72653b154639272a6a46d0d1f8b85
YanuarGuo <yanuar.utama14@gmail.com>
2025-03-18 10:16:12 +0700

    Finishing project

--- TripleDESEncryption.Designer.cs
+++ TripleDESEncryption.Designer.cs
@@ -28,12 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "TripleDESEncryption";
+            BtnEncrypt = new Button();
+            BtnDecrypt = new Button();
+            TxtPlain = new TextBox();
+            TxtCipher = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // BtnEncrypt
+            // 
+            BtnEncrypt.Location = new Point(264, 129);
+            BtnEncrypt.Name = "BtnEncrypt";
+            BtnEncrypt.Size = new Size(75, 23);
+            BtnEncrypt.TabIndex = 0;
+            BtnEncrypt.Text = "Encrypt";
+            BtnEncrypt.UseVisualStyleBackColor = true;
+            BtnEncrypt.Click += BtnEncrypt_Click;
+            // 
+            // BtnDecrypt
+            // 
+            BtnDecrypt.Location = new Point(183, 129);
+            BtnDecrypt.Name = "BtnDecrypt";
+            BtnDecrypt.Size = new Size(75, 23);
+            BtnDecrypt.TabIndex = 1;
+            BtnDecrypt.Text = "Decrypt";
+            BtnDecrypt.UseVisualStyleBackColor = true;
+            BtnDecrypt.Click += BtnDecrypt_Click;
+            // 
+            // TxtPlain
+            // 
+            TxtPlain.Location = new Point(12, 37);
+            TxtPlain.Name = "TxtPlain";
+            TxtPlain.Size = new Size(327, 23);
+            TxtPlain.TabIndex = 2;
+            // 
+            // TxtCipher
+            // 
+            TxtCipher.Location = new Point(12, 90);
+            TxtCipher.Name = "TxtCipher";
+            TxtCipher.Size = new Size(327, 23);
+            TxtCipher.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(57, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Plain Text";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 72);
+            label2.Name = "label2";
+            label2.Size = new Size(66, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Cipher Text";
+            // 
+            // TripleDESEncryption
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(351, 168);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(TxtCipher);
+            Controls.Add(TxtPlain);
+            Controls.Add(BtnDecrypt);
+            Controls.Add(BtnEncrypt);
+            Name = "TripleDESEncryption";
+            Text = "TripleDESEncryption";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button BtnEncrypt;
+        private Button BtnDecrypt;
+        private TextBox TxtPlain;
+        private TextBox TxtCipher;
+        private Label label1;
+        private Label label2;
     }
 }
--- TripleDESEncryption.cs
+++ TripleDESEncryption.cs
@@ -1,10 +1,69 @@
+using System;
+using System.Configuration;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
 namespace TripleDESEncryption
 {
     public partial class TripleDESEncryption : Form
     {
+        private static readonly string Key =
+            ConfigurationManager.AppSettings["Key"] ?? string.Empty;
+
         public TripleDESEncryption()
         {
             InitializeComponent();
+        }
+
+        private void BtnEncrypt_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(TxtPlain.Text))
+            {
+                MessageBox.Show("Please enter some text to encrypt.");
+                return;
+            }
+            TxtCipher.Text = TripleDesEncrypt(TxtPlain.Text);
+        }
+
+        private void BtnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(TxtCipher.Text))
+            {
+                MessageBox.Show("Please enter some text to decrypt.");
+                return;
+            }
+            TxtPlain.Text = TripleDesDecrypt(TxtCipher.Text);
+        }
+
+        public static string TripleDesEncrypt(string plainText)
+        {
+            var des = CreateDes(Key);
+            var ct = des.CreateEncryptor();
+            var input = Encoding.UTF8.GetBytes(plainText);
+            var output = ct.TransformFinalBlock(input, 0, input.Length);
+            return Convert.ToBase64String(output);
+        }
+
+        public static string TripleDesDecrypt(string cypherText)
+        {
+            var des = CreateDes(Key);
+            var ct = des.CreateDecryptor();
+            var input = Convert.FromBase64String(cypherText);
+            var output = ct.TransformFinalBlock(input, 0, input.Length);
+            return Encoding.UTF8.GetString(output);
+        }
+
+        public static TripleDES CreateDes(string key)
+        {
+            using MD5 md5 = MD5.Create();
+            TripleDES des = TripleDES.Create();
+            var desKey = md5.ComputeHash(Encoding.UTF8.GetBytes(key));
+            des.Key = desKey;
+            des.IV = new byte[des.BlockSize / 8];
+            des.Padding = PaddingMode.PKCS7;
+            des.Mode = CipherMode.ECB;
+            return des;
         }
     }
 }
